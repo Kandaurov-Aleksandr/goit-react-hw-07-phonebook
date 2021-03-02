2ddd9b60c65b722be43964faf4b266c8ef3f05a6
Kandaurov-Aleksandr <kandayrov1990@gmail.com>
2021-03-02 15:05:58 +0200

    fixed hw-07

--- src/index.js
+++ src/index.js
@@ -1,20 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-// import { PersistGate } from 'redux-persist/integration/react';
-// import { store, persistor } from './redux/store';
-import store from './redux/store';
-
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/App.js";
+import { Provider } from "react-redux";
+import store from "./redux/store";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <Provider store={store}>
-      {/* <PersistGate loading={null} persistor={persistor}> */}
-      <App />
-      {/* </PersistGate> */}
-    </Provider>
-  </React.StrictMode>,
-  document.getElementById('root'),
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
 );
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,39 +1,9 @@
-import { configureStore } from '@reduxjs/toolkit';
-import contactsReducer from './contacts.js/contacts-reducer';
-import {
-  // persistStore,
-  // persistReducer,
-  FLUSH,
-  REHYDRATE,
-  PAUSE,
-  PERSIST,
-  PURGE,
-  REGISTER,
-} from 'redux-persist';
-// import storage from 'redux-persist/lib/storage';
-import logger from 'redux-logger';
+import app from "../redux/contacts/contactsReducer";
+
+import { configureStore } from "@reduxjs/toolkit";
 
 const store = configureStore({
-  reducer: {
-    contacts: contactsReducer,
-  },
-  middleware: getDefaultMiddleware =>
-    getDefaultMiddleware({
-      serializableCheck: {
-        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
-      },
-    }).concat(logger),
-  devTools: process.env.NODE_ENV !== 'production',
+  reducer: { app },
 });
 
-// const persistor = persistStore(store);
-// export { store, persistor };
 export default store;
-
-// ***Redux***
-
-// import { createStore, combineReducers } from 'redux';
-// import { combineReducers } from 'redux';
-// import { composeWithDevTools } from 'redux-devtools-extension';
-// const store = createStore(rooReducer, composeWithDevTools());
-// const rooReducer = combineReducers({contacts: contactsReducer});
